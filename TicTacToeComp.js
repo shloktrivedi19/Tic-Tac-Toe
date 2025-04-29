2e24b06bbdb42f72d57f7bc04625f5359a0e006c
shloktrivedi19 <shlokprakash@19gmail.com>
2025-04-29 17:00:55 +0530

    Update AI Move

--- TicTacToeComp.js
+++ TicTacToeComp.js
@@ -1,196 +1,192 @@
 let boxes = document.querySelectorAll(".box");
 let turn = 0;
 let arr = Array.from(boxes);
-let winningDec = [[0, 1, 2] , [3, 4, 5] , [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]  // winning possibilities
-let newArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let winningDec = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+];
 let winnerDeclare = false;
 let won = document.querySelector(".winner");
 let resetGame = document.querySelector(".reset-game");
 
-let compTurn = (i) => {                                  // Generating AI's move
-    if (turn === 1) {
-        let number = Math.floor(Math.random() * 9);
-        if (number === i) {
-            while (number === i) {
-                number = Math.floor(Math.random() * 9);
-            }
-            boxes[number].innerText = "X";
-            boxes[number].style.pointerEvents = "none";
-            boxes[number].disabled = true;
-            turn++;
-            win();
-        } else {
-            boxes[number].innerText = "X";
-            boxes[number].disabled = true;
-            boxes[number].style.pointerEvents = "none";
-            turn++;
-            if (!winnerDeclare) {
-                boxes.forEach((box) => {
-                    if (box.innerText === "") {
-                        box.style.pointerEvents = "auto";
-                        box.disabled = false;
-                    }
-                });
-            }
-            win();
-        }
-    } else if (turn > 1) {
-        // First, try to win
-        for (let card of winningDec) {
-            let val1 = boxes[card[0]];
-            let val2 = boxes[card[1]];
-            let val3 = boxes[card[2]];
-            let values = [val1 , val2 , val3];
-            
-            if (
-                (val1.innerText === "X" && val2.innerText === "X" && val3.innerText === "") || 
-                (val2.innerText === "X" && val3.innerText === "X" && val1.innerText === "") ||
-                (val3.innerText === "X" && val1.innerText === "X" && val2.innerText === "")
-            ) {
-                for (let value of values) {
-                    if (value.innerText === "") {
-                        value.innerText = "X";
-                        value.style.pointerEvents = "none";
-                        value.disabled = true;
-                        turn++;
-                        if (!winnerDeclare) {
-                            boxes.forEach((box) => {
-                                if (box.innerText === "") {
-                                    box.style.pointerEvents = "auto";
-                                    box.disabled = false;
-                                }
-                            });
-                        }
-                        win();
-                        return;
-                    }
-                }
-            }
-        }
-    
-        // If no winning move, try to block
-        for (let card of winningDec) {
-            let val1 = boxes[card[0]];
-            let val2 = boxes[card[1]];
-            let val3 = boxes[card[2]];
-            let values = [val1 , val2 , val3];
-            
-            if (
-                (val1.innerText === "O" && val2.innerText === "O" && val3.innerText === "") || 
-                (val2.innerText === "O" && val3.innerText === "O" && val1.innerText === "") ||
-                (val3.innerText === "O" && val1.innerText === "O" && val2.innerText === "")
-            ) {
-                for (let value of values) {
-                    if (value.innerText === "") {
-                        value.innerText = "X";
-                        value.style.pointerEvents = "none";
-                        value.disabled = true;
-                        turn++;
-                        if (!winnerDeclare) {
-                            boxes.forEach((box) => {
-                                if (box.innerText === "") {
-                                    box.style.pointerEvents = "auto";
-                                    box.disabled = false;
-                                }
-                            });
-                        }
-                        win();
-                        return;
-                    }
-                }
-            }
-        }
-    
-        // Else, random move
-        let nullArr = Array.from(boxes).filter((i) => i.innerText === "");
-        let num = Math.floor(Math.random() * nullArr.length);
-        nullArr[num].innerText = "X";
-        nullArr[num].disabled = true;
-        nullArr[num].style.pointerEvents = "none";
-        turn++;
-        if (!winnerDeclare) {
-            boxes.forEach((box) => {
-                if (box.innerText === "") {
-                    box.style.pointerEvents = "auto";
-                    box.disabled = false;
-                }
-            });
-        }
-        win();
-    }    
-}
+let compTurn = (i) => {
+  if (winnerDeclare) return;
 
-let resetGamefunc = () => {                     // setting up the reset button
-    winnerDeclare = false;
-    for (let box of boxes) {
-        box.disabled = false;
-        box.innerText = "";
-        box.style.border = "none";
-        box.style.pointerEvents = "";
-        won.classList.add("hide");
-        turn = 0;
+  if (turn === 1) {
+    let number = Math.floor(Math.random() * 9);
+    while (number === i || boxes[number].innerText !== "") {
+      number = Math.floor(Math.random() * 9);
     }
-}
+    boxes[number].innerText = "X";
+    boxes[number].style.pointerEvents = "none";
+    boxes[number].disabled = true;
+    turn++;
+    win();
+    return;
+  }
 
-let win = () => {                                 // checking for a win
-    for (let i of winningDec) {
-        let pos1 = boxes[i[0]].innerText;
-        let pos2 = boxes[i[1]].innerText;
-        let pos3 = boxes[i[2]].innerText;
-
-        if (pos1 !== "" && pos1 === pos2 && pos2 === pos3) {
-                if (pos1 === "X") {
-                    won.style.backgroundColor = "red";
-                    won.innerText = "AI Won!"
-                    won.classList.remove("hide");
-                    boxes[i[0]].style.border = "7px solid red";
-                    boxes[i[1]].style.border = "7px solid red";
-                    boxes[i[2]].style.border = "7px solid red";
-                } else {
-                    won.style.backgroundColor = "rgb(211, 170, 64)";
-                    won.innerText = "Congratulations, You Won!"
-                    won.classList.remove("hide");
-                    boxes[i[0]].style.border = "7px solid goldenrod";
-                    boxes[i[1]].style.border = "7px solid goldenrod";
-                    boxes[i[2]].style.border = "7px solid goldenrod";
-                }
-                winnerDeclare = true;
-                for (let box of boxes) {
-                    box.disabled = true;
-                }
-            }
-        }
-        if (turn === 9) {
-            won.style.backgroundColor = "gray";
-            won.innerText = "Game was Draw!";
-            won.classList.remove("hide");
-            winnerDeclare = true;
-                for (let box of boxes) {
-                    box.disabled = true;
-                }
-        }
-}
-
-boxes.forEach((box) => {                              // Adding Event to start the game.
-    box.addEventListener("click" , () => {
-        box.innerText = "O";
-        box.style.pointerEvents = "none";
-        box.disabled = true;
+  if (turn > 1) {
+    // Try to win
+    for (let card of winningDec) {
+      let [a, b, c] = card;
+      if (
+        boxes[a].innerText === "X" &&
+        boxes[b].innerText === "X" &&
+        boxes[c].innerText === ""
+      ) {
+        boxes[c].innerText = "X";
+        boxes[c].style.pointerEvents = "none";
+        boxes[c].disabled = true;
         turn++;
-        let val = Number(box.getAttribute("id"));
         win();
+        return;
+      } else if (
+        boxes[b].innerText === "X" &&
+        boxes[c].innerText === "X" &&
+        boxes[a].innerText === ""
+      ) {
+        boxes[a].innerText = "X";
+        boxes[a].style.pointerEvents = "none";
+        boxes[a].disabled = true;
+        turn++;
+        win();
+        return;
+      } else if (
+        boxes[c].innerText === "X" &&
+        boxes[a].innerText === "X" &&
+        boxes[b].innerText === ""
+      ) {
+        boxes[b].innerText = "X";
+        boxes[b].style.pointerEvents = "none";
+        boxes[b].disabled = true;
+        turn++;
+        win();
+        return;
+      }
+    }
+
+    // Try to block
+    for (let card of winningDec) {
+      let [a, b, c] = card;
+      if (
+        boxes[a].innerText === "O" &&
+        boxes[b].innerText === "O" &&
+        boxes[c].innerText === ""
+      ) {
+        boxes[c].innerText = "X";
+        boxes[c].style.pointerEvents = "none";
+        boxes[c].disabled = true;
+        turn++;
+        win();
+        return;
+      } else if (
+        boxes[b].innerText === "O" &&
+        boxes[c].innerText === "O" &&
+        boxes[a].innerText === ""
+      ) {
+        boxes[a].innerText = "X";
+        boxes[a].style.pointerEvents = "none";
+        boxes[a].disabled = true;
+        turn++;
+        win();
+        return;
+      } else if (
+        boxes[c].innerText === "O" &&
+        boxes[a].innerText === "O" &&
+        boxes[b].innerText === ""
+      ) {
+        boxes[b].innerText = "X";
+        boxes[b].style.pointerEvents = "none";
+        boxes[b].disabled = true;
+        turn++;
+        win();
+        return;
+      }
+    }
+
+    // Random move
+    let nullArr = Array.from(boxes).filter((i) => i.innerText === "");
+    if (nullArr.length > 0) {
+      let num = Math.floor(Math.random() * nullArr.length);
+      nullArr[num].innerText = "X";
+      nullArr[num].disabled = true;
+      nullArr[num].style.pointerEvents = "none";
+      turn++;
+      win();
+    }
+  }
+};
+
+let resetGamefunc = () => {
+  winnerDeclare = false;
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+    box.style.border = "none";
+    box.style.pointerEvents = "";
+  }
+  won.classList.add("hide");
+  turn = 0;
+};
+
+let win = () => {
+  for (let i of winningDec) {
+    let [a, b, c] = i;
+    let pos1 = boxes[a].innerText;
+    let pos2 = boxes[b].innerText;
+    let pos3 = boxes[c].innerText;
+
+    if (pos1 !== "" && pos1 === pos2 && pos2 === pos3) {
+      winnerDeclare = true;
+      if (pos1 === "X") {
+        won.style.backgroundColor = "red";
+        won.innerText = "AI Won!";
+        boxes[a].style.border = boxes[b].style.border = boxes[c].style.border = "7px solid red";
+      } else {
+        won.style.backgroundColor = "rgb(211, 170, 64)";
+        won.innerText = "Congratulations, You Won!";
+        boxes[a].style.border = boxes[b].style.border = boxes[c].style.border = "7px solid goldenrod";
+      }
+      won.classList.remove("hide");
+      boxes.forEach((box) => box.disabled = true);
+      return;
+    }
+  }
+
+  if (!winnerDeclare && turn === 9) {
+    winnerDeclare = true;
+    won.style.backgroundColor = "gray";
+    won.innerText = "Game was Draw!";
+    won.classList.remove("hide");
+    boxes.forEach((box) => box.disabled = true);
+  }
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (winnerDeclare || box.innerText !== "") return;
+
+    box.innerText = "O";
+    box.style.pointerEvents = "none";
+    box.disabled = true;
+    turn++;
+    let val = Number(box.getAttribute("id"));
+    win();
+    boxes.forEach((b) => b.style.pointerEvents = "none");
+
+    setTimeout(() => {
+      if (!winnerDeclare) {
+        compTurn(val);
         boxes.forEach((b) => {
-            b.style.pointerEvents = "none";
+          if (b.innerText === "") {
+            b.style.pointerEvents = "auto";
+            b.disabled = false;
+          }
         });
-        setTimeout(() => {
-            if (!winnerDeclare) {
-                compTurn(val)
-            }
-        }, 1000);
-    })
-}) 
+      }
+    }, 1000);
+  });
+});
 
-resetGame.addEventListener("click" , () => {               //adding event for reset button
-    resetGamefunc()
-})
-
+resetGame.addEventListener("click", resetGamefunc);
